3dcb49c5643a9b395541e175737987d2f25b63a3
Ryan Carney <arecarn@gmail.com>
2020-07-12 05:05:07 -0700

    cmake: remove broken format_check target, and cleanup format

--- tools/cmake/clangFormat.cmake
+++ tools/cmake/clangFormat.cmake
@@ -11,28 +11,8 @@ function(addClangFormatTargets)
     if(CLANG_FORMAT)
         add_custom_target(
             format
-            COMMAND
-            ${CLANG_FORMAT}
-            -i
-            -style=file
-            ${ARGS_FILES}
+            COMMAND ${CLANG_FORMAT} -i -style=file ${ARGS_FILES}
             COMMENT "Auto formatting of all source files"
-            )
-
-        add_custom_target(
-            format_check
-            COMMAND
-            ${CLANG_FORMAT}
-            -style=file
-            -output-replacements-xml
-            ${ARGS_FILES}
-            # print output
-            | tee ${CMAKE_BINARY_DIR}/check_format_file.txt | grep -c "replacement " |
-            tr -d "[:cntrl:]" && echo " replacements necessary"
-            # WARNING: fix to stop with error if there are problems
-            COMMAND ! grep -c "replacement "
-            ${CMAKE_BINARY_DIR}/check_format_file.txt > /dev/null
-            COMMENT "Checking format compliance"
             )
     endif()
 endfunction()
