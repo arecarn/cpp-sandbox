620a09c7e7c4dc81cc9ca88c6d16f91dce47e8eb
Ryan Carney <arecarn@gmail.com>
2020-07-27 10:39:27 -0700

    tagged_union: add some additional sanity check tests

--- src/tagged_union/tagged_union.test.cpp
+++ src/tagged_union/tagged_union.test.cpp
@@ -5,4 +5,13 @@ TEST(basic, pizza)
 {
     constexpr int N = 5;
     Value v{N};
+    ASSERT_EQ(*v.as_int(), N);
+
+    constexpr int K = 6;
+    v.as_int(K);
+    ASSERT_EQ(*v.as_int(), K);
+
+    constexpr float F = 5.5;
+    v.as_float(F);
+    ASSERT_EQ(*v.as_float(), F);
 }
