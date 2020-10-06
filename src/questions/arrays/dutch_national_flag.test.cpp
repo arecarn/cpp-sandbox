a57d7bccb5017083b835785bf83a46d0e0863de6
Ryan Carney <arecarn@gmail.com>
2020-10-05 20:56:27 -0700

    questions: dutch_national_flag: add a solution

--- src/questions/arrays/dutch_national_flag.test.cpp
+++ src/questions/arrays/dutch_national_flag.test.cpp
@@ -1,41 +1,66 @@
-
-#include <gtest/gtest.h>
 #include <array>
+#include <gtest/gtest.h>
+#include <iostream>
 
-template<typename T, size_t Size>
-void dutch_national_flag(std::array<T,Size>& arr, size_t pivot_index)
+template <typename T>
+void dutch_national_flag(std::vector<T>& arr, size_t pivot_index)
 {
-    const atuo pivot = arr[pivot_index];
-    int i = 0;
-    int k = Size -1;
-    while (i < k)
+    const auto pivot = arr[pivot_index];
+    auto b = arr.begin();
+    auto e = arr.end() - 1;
+
+    auto f = [](auto b, auto e, T pivot) -> auto
     {
-        if (arr[i] <= pivot);
+        while (b < e)
         {
-            std::swap(arr[i], arr[k]);
-            k--;
+            if (*b > pivot)
+            {
+
+                if (*e <= pivot)
+                {
+                    std::swap(*b, *e);
+                }
+                e--;
+            }
+            else
+            {
+                b++;
+            }
         }
-        else
+
+        if (*b == pivot)
         {
-            i++;
+            return b;
         }
-    }
+        return e;
+    };
+    auto p = f(b, e, pivot);
+    f(b, p, pivot - 1);
 }
 
 TEST(dutch_national_flag, one)
 {
-    constexpr size_t Pivot {3};
-    std::array<int,7> input {0, 1, 2, 0, 2, 1, 1};
+    constexpr size_t Pivot {1};
+    std::vector<int> input {0, 1, 2, 0, 2, 1, 1};
     dutch_national_flag(input, Pivot);
-    const std::array<int,7> expected {0, 0, 1, 1, 1, 2, 2};
+    const std::vector<int> expected {0, 0, 1, 1, 1, 2, 2};
     ASSERT_EQ(input, expected);
 }
 
-// TEST(dutch_national_flag, two)
-// {
-//     constexpr size_t Pivot {3};
-//     std::array<int,7> input {0, 1, 2, 0, 2, 1, 1};
-//     dutch_national_flag(input, Pivot);
-//     const std::array<int,7> expected {0, 0, 1, 1, 1, 2, 2};
-//     ASSERT_EQ(input, expected);
-// }
+TEST(dutch_national_flag, two)
+{
+    constexpr size_t Pivot {2};
+    std::vector<int> input {0, 1, 2, 0, 2, 1, 1};
+    dutch_national_flag(input, Pivot);
+    const std::vector<int> expected {0, 1, 1, 0, 1, 2, 2};
+    ASSERT_EQ(input, expected);
+}
+
+TEST(dutch_national_flag, zero)
+{
+    constexpr size_t Pivot {0};
+    std::vector<int> input {0, 1, 2, 0, 2, 1, 1};
+    dutch_national_flag(input, Pivot);
+    const std::vector<int> expected {0, 0, 2, 1, 2, 1, 1};
+    ASSERT_EQ(input, expected);
+}
