476e8d999624acda4708dfbe55f9e96db4fc3586
Ryan Carney <arecarn@gmail.com>
2020-12-30 11:29:51 -0800

    ringbuffer: make double ended e.g. push_front, pop_front, push_back, pop_back

--- src/ringbuffer/ringbuffer.hpp
+++ src/ringbuffer/ringbuffer.hpp
@@ -2,6 +2,7 @@
 #define RINGBUFFER_HPP
 
 #include <cstddef>
+#include <cstdint>
 
 template <typename T, size_t N>
 class ringbuffer
@@ -12,32 +13,101 @@ public:
     ringbuffer() = default;
     ~ringbuffer() = default;
 
-    bool push(const T x)
+    bool push_back(const T x)
     {
         if (m_size == N)
         {
             return false;
         }
-        m_data[m_write_index] = x;
-        ++m_write_index;
-        if (m_write_index == N)
+
+        if (m_front == -1) // queue is empty
         {
-            m_write_index = 0;
+            m_front = 0;
+            m_back = 0;
         }
+        else if (static_cast<size_t>(m_back) == Max_Index) // m_back at last position
+        {
+            m_back = 0;
+        }
+        else
+        {
+            ++m_back;
+        }
+
+        m_data[m_back] = x;
         ++m_size;
         return true;
     }
 
-    bool pop()
+    bool push_front(const T x)
+    {
+        if (m_size == N)
+        {
+            return false;
+        }
+
+        if (m_front == -1) // If queue is initially empty
+        {
+            m_front = 0;
+            m_back = 0;
+        }
+        else if (m_front == 0) // m_front at first position
+        {
+            m_front = Max_Index;
+        }
+        else
+        {
+            --m_front;
+        }
+
+        ++m_size;
+        m_data[m_front] = x;
+        return true;
+    }
+
+    bool pop_front()
     {
         if (m_size == 0)
         {
             return false;
         }
-        ++m_read_index;
-        if (m_read_index == N)
+
+        if (m_front == m_back) // has only one element
         {
-            m_read_index = 0;
+            m_front = -1;
+            m_back = -1;
+        }
+        else if (static_cast<size_t>(m_front) == Max_Index) // back to initial position
+        {
+            m_front = 0;
+        }
+        else
+        {
+            ++m_front;
+        }
+        --m_size;
+        return true;
+    }
+
+    bool pop_back()
+    {
+        if (m_size == 0)
+        {
+            return false;
+        }
+
+        if (m_front == m_back) // only one element
+        {
+            m_front = -1;
+            m_back = -1;
+        }
+        else if (m_back == 0)
+        {
+            m_back = Max_Index;
+        }
+        else
+        {
+            --m_back;
         }
         --m_size;
         return true;
@@ -45,16 +115,16 @@ public:
 
     T& operator[](const size_t i)
     {
-        size_t j = (m_read_index + i) % N;
+        size_t j = (static_cast<size_t>(m_front) + i) % N;
         return m_data[j];
     }
 
-    size_t size() const
+    [[nodiscard]] size_t size() const
     {
         return m_size;
     }
 
-    bool empty() const
+    [[nodiscard]] bool empty() const
     {
         return m_size == 0;
     }
@@ -68,8 +138,8 @@ private:
     static constexpr size_t Capacity {N};
     static constexpr size_t Max_Index {N - 1};
     value_type m_data[N] {};
-    size_t m_write_index {0};
-    size_t m_read_index {0};
+    int32_t m_back {0};
+    int32_t m_front {-1};
     size_t m_size {0};
 };
 
--- src/ringbuffer/ringbuffer.test.cpp
+++ src/ringbuffer/ringbuffer.test.cpp
@@ -1,72 +1,142 @@
 #include "ringbuffer.hpp"
 #include "gtest/gtest.h"
 
-TEST(ringbuffer, push)
+TEST(ringbuffer, push_back)
 {
     ringbuffer<int, 3> rb;
 
     ASSERT_EQ(rb.capacity(), 3);
     ASSERT_EQ(rb.size(), 0);
 
-    ASSERT_EQ(true, rb.push(1));
+    ASSERT_EQ(true, rb.push_back(1));
     ASSERT_EQ(rb.size(), 1);
 
-    ASSERT_EQ(true, rb.push(2));
+    ASSERT_EQ(true, rb.push_back(2));
     ASSERT_EQ(rb.size(), 2);
 
-    ASSERT_EQ(true, rb.push(3));
+    ASSERT_EQ(true, rb.push_back(3));
     ASSERT_EQ(rb.size(), 3);
 
     ASSERT_EQ(1, rb[0]);
     ASSERT_EQ(2, rb[1]);
     ASSERT_EQ(3, rb[2]);
 
-    ASSERT_EQ(false, rb.push(4));
+    ASSERT_EQ(false, rb.push_back(4));
     ASSERT_EQ(rb.size(), 3);
 }
 
-TEST(ringbuffer, pop)
+TEST(ringbuffer, push_front)
 {
     ringbuffer<int, 3> rb;
-    rb.push(1);
-    rb.push(2);
-    rb.push(3);
 
-    ASSERT_EQ(true, rb.pop());
+    ASSERT_EQ(rb.capacity(), 3);
+    ASSERT_EQ(rb.size(), 0);
+
+    ASSERT_EQ(true, rb.push_front(1));
+    ASSERT_EQ(rb.size(), 1);
+
+    ASSERT_EQ(true, rb.push_front(2));
+    ASSERT_EQ(rb.size(), 2);
+
+    ASSERT_EQ(true, rb.push_front(3));
+    ASSERT_EQ(rb.size(), 3);
+
+    ASSERT_EQ(3, rb[0]);
+    ASSERT_EQ(2, rb[1]);
+    ASSERT_EQ(1, rb[2]);
+
+    ASSERT_EQ(false, rb.push_front(4));
+    ASSERT_EQ(rb.size(), 3);
+}
+
+TEST(ringbuffer, pop_front)
+{
+    ringbuffer<int, 3> rb;
+    rb.push_back(1);
+    rb.push_back(2);
+    rb.push_back(3);
+
+    ASSERT_EQ(true, rb.pop_front());
     ASSERT_EQ(2, rb[0]);
     ASSERT_EQ(3, rb[1]);
     ASSERT_EQ(rb.size(), 2);
 
-    ASSERT_EQ(true, rb.pop());
+    ASSERT_EQ(true, rb.pop_front());
     ASSERT_EQ(3, rb[0]);
     ASSERT_EQ(rb.size(), 1);
 
-    ASSERT_EQ(true, rb.pop());
+    ASSERT_EQ(true, rb.pop_front());
     ASSERT_EQ(rb.size(), 0);
 }
 
-TEST(ringbuffer, push_wrap)
+TEST(ringbuffer, pop_back)
 {
     ringbuffer<int, 3> rb;
-    rb.push(1);
-    rb.push(2);
-    rb.push(3);
+    rb.push_back(1);
+    rb.push_back(2);
+    rb.push_back(3);
 
-    // push wraps around to front
-    rb.pop();
-    rb.push(4);
+    ASSERT_EQ(true, rb.pop_back());
+    ASSERT_EQ(1, rb[0]);
+    ASSERT_EQ(2, rb[1]);
+    ASSERT_EQ(rb.size(), 2);
+
+    ASSERT_EQ(true, rb.pop_back());
+    ASSERT_EQ(1, rb[0]);
+    ASSERT_EQ(rb.size(), 1);
+
+    ASSERT_EQ(true, rb.pop_back());
+    ASSERT_EQ(rb.size(), 0);
+}
+
+TEST(ringbuffer, push_back_wrap)
+{
+    ringbuffer<int, 3> rb;
+    rb.push_back(1);
+    rb.push_back(2);
+    rb.push_back(3);
+
+    // push_back wraps around to front
+    rb.pop_front();
+    rb.push_back(4);
     ASSERT_EQ(2, rb[0]);
     ASSERT_EQ(3, rb[1]);
     ASSERT_EQ(4, rb[2]);
 
-    rb.pop();
+    rb.pop_front();
     ASSERT_EQ(3, rb[0]);
     ASSERT_EQ(4, rb[1]);
 
-    // pop wraps around to the front
-    rb.pop();
+    // pop_front wraps around to the front
+    rb.pop_front();
     ASSERT_EQ(4, rb[0]);
 
-    rb.pop();
+    rb.pop_front();
+    ASSERT_TRUE(rb.empty());
+}
+
+TEST(ringbuffer, push_front_wrap)
+{
+    ringbuffer<int, 3> rb;
+    rb.push_front(1);
+    rb.push_front(2);
+    rb.push_front(3);
+
+    // push_back wraps around to front
+    rb.pop_front();
+    rb.push_front(4);
+    ASSERT_EQ(4, rb[0]);
+    ASSERT_EQ(2, rb[1]);
+    ASSERT_EQ(1, rb[2]);
+
+    rb.pop_front();
+    ASSERT_EQ(2, rb[0]);
+    ASSERT_EQ(1, rb[1]);
+
+    // pop_front wraps around to the front
+    rb.pop_front();
+    ASSERT_EQ(1, rb[0]);
+
+    rb.pop_front();
     ASSERT_TRUE(rb.empty());
 }
